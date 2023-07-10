384ee849f51cda6d9f52ddac3726c6afaf9cc113
unknown <development@vero-as.com>
2023-07-10 21:04:19 +0300

    ddddddddddd

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FirstPage></FirstPage>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+import FirstPage from './components/FirstPage.vue';
+
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,FirstPage
   }
 }
 </script>
